16873af6a165558632312ba64be7dc74686b4805
JakubowskiA <arieljakubowski@gmail.com>
2019-09-09 09:32:37 -0400

    Added accordion table

--- src/components/StressLevels.js
+++ src/components/StressLevels.js
@@ -1,11 +1,112 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 
 const StressLevels=()=>{
     return(
     <div >
         {/* <h1 >Stress Scale</h1> */}
         <br/><br/>
-        <table border="1" className="rounded" style={{'borderRadius':'1%!important'}}>
+
+    <div className="accordion">
+    <h2>Stress Scale</h2>
+    <p>Click on a level to see more</p>
+    <Collapsible trigger="Level 1">
+        <p triggerTagName="Level 1" className="panel"> 
+        <ul>
+            <li>Total peace</li>
+        </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 2">
+        <p triggerTagName="Level 2" className="panel"> 
+        <ul>
+            <li>Feeling slightly stressed</li>
+        </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 3">
+        <p triggerTagName="Level 3" className="panel"> 
+        <ul>
+                        <li>Feeling slightly stressed</li>
+                        <li>Difficulty concentrating</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 4">
+        <p triggerTagName="Level 4" className="panel"> 
+        <ul>
+                        <li>Feeling moderately stressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 5">
+        <p triggerTagName="Level 5" className="panel"> 
+        <ul>
+                        <li>Feeling moderately stressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                        <li>Feeling irritable</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 6">
+        <p triggerTagName="Level 6" className="panel"> 
+        <ul>
+                        <li>Feeling severely stressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                        <li>Feeling a lack of control</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 7">
+        <p triggerTagName="Level 7" className="panel"> 
+        <ul>
+                        <li>Feeling severely stressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                        <li>Feeling a lack of control</li>
+                        <li>One physical symptom</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 8">
+        <p triggerTagName="Level 8" className="panel"> 
+        <ul>
+                        <li>Feeling distressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                        <li>Feeling a lack of control</li>
+                        <li>Dissociation</li>
+                        <li>Two physical symptoms</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 9">
+        <p triggerTagName="Level 9" className="panel"> 
+        <ul>
+                        <li>Feeling distressed</li>
+                        <li>Difficulty concentrating</li>
+                        <li>Feeling overwhelmed</li>
+                        <li>Feeling a lack of control</li>
+                        <li>Dissociation</li>
+                        <li>Tunnel vision</li>
+                        <li>Multiple physical symptoms</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    <Collapsible trigger="Level 10">
+        <p triggerTagName="Level 10" className="panel"> 
+        <ul>
+                        <li>Panic attack</li>
+                    </ul>
+        </p>
+    </Collapsible>
+    </div>
+
+        {/* <table border="1" className="rounded" style={{'borderRadius':'1%!important'}}>
             <thead>
             <tr>
                 <th className="centered-table-element">Stress Level</th>
@@ -119,18 +220,12 @@ const StressLevels=()=>{
             </tr>
             
             </tbody>
-    </table>
+    </table> */}
         <br/>
-    <table border="1">
-            <thead>
-            <tr>
-                <th className="centered-table-element">Physical Symptoms of Stress</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td >
-                    <ul className="symptoms-table">
+        <div>
+            <div className="symptoms-table-outer">
+                <h3>Physical Symptoms of Stress</h3>
+            <ul className="symptoms-table">
                         <li>Fast heartbeat</li>
                         <li>Shaking</li>
                         <li>Chest pain</li>
@@ -141,11 +236,9 @@ const StressLevels=()=>{
                         <li>Neck or back pain</li>
                         <li>Abnormal breathing</li>
                     </ul>
-                </td>
-            </tr>
-            
-            </tbody>
-    </table>
+            </div>
+        </div>
+    
     <br/>
     </div>
     )

db694426016db48549b629758ba0a318e2cefbc8
JakubowskiA <arieljakubowski@gmail.com>
2019-09-11 13:24:46 -0400

    Added background options

--- src/components/BackgroundSelectModal.js
+++ src/components/BackgroundSelectModal.js
@@ -20,15 +20,17 @@ function BackgroundSelectModal (props) {
     <button onClick={handleShow} className="background-btn"> </button>
     <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
+          <p>Choose your background.</p>
         </Modal.Header>
         <Modal.Body>
-          <p>Choose your background.</p>
           <div className="background-container" onClick={(event)=>props.customizeBackground(event)}>
           <div className="rainbow-btn" id="rainbow" ><br/></div>
           <br/>
           <div className="blue-green-btn" id="blue-green"><br/></div>
           <br/> <br/>
           <div className="bgp-btn" id="bgp"><br/></div>
+          <br/> <br/>
+          <div className="blue-neutral-btn" id="blue-neutral"><br/></div>
           <br/> <br/>
           </div>
           
